5ba0fd4ac206c2a796df3427a2ef018f37e98d18
Edwin <edwin.barrera1025@gmail.com>
2026-06-10 15:50:23 +0200

    ApllicationController: adding conditional to ensure that beforeFilter runs only when at least one parameter is passed

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -23,52 +23,54 @@ class ApplicationController extends Controller
     {
 
         $initialParms = parent::_getAllParams();
-        $validations = [
-            'id' => [
-                'filter' => FILTER_VALIDATE_REGEXP,
-                'options' => [
-                    'regexp' => '/^[a-z0-9]+$/'
+        if (count($initialParms) > 0) {
+            $validations = [
+                'id' => [
+                    'filter' => FILTER_VALIDATE_REGEXP,
+                    'options' => [
+                        'regexp' => '/^[a-z0-9]+$/'
+                    ]
+                ],
+                'status' => FILTER_UNSAFE_RAW,
+                'from' => [
+                    'filter' => FILTER_VALIDATE_REGEXP,
+                    'options' => [
+                        'regexp' => '/^([01][0-9]|2[0-3]):[0-5][0-9]$/'
+                    ]
+                ],
+                'to' =>  [
+                    'filter' => FILTER_VALIDATE_REGEXP,
+                    'options' => [
+                        'regexp' => '/^([01][0-9]|2[0-3]):[0-5][0-9]$/'
+                    ]
+                ],
+                'keywords' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+                'createdBy' => [
+                    'filter' => FILTER_VALIDATE_REGEXP,
+                    'options' => [
+                        'regexp' => '/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+$/'
+                    ]
                 ]
-            ],
-            'status' => FILTER_UNSAFE_RAW,
-            'from' => [
-                'filter' => FILTER_VALIDATE_REGEXP,
-                'options' => [
-                    'regexp' => '/^([01][0-9]|2[0-3]):[0-5][0-9]$/'
-                ]
-            ],
-            'to' =>  [
-                'filter' => FILTER_VALIDATE_REGEXP,
-                'options' => [
-                    'regexp' => '/^([01][0-9]|2[0-3]):[0-5][0-9]$/'
-                ]
-            ],
-            'keywords' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-            'createdBy' => [
-                'filter' => FILTER_VALIDATE_REGEXP,
-                'options' => [
-                    'regexp' => '/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+$/'
-                ]
-            ]
-        ];
-        $validatedParms = filter_var_array($initialParms, $validations);
+            ];
+            $validatedParms = filter_var_array($initialParms, $validations);
 
-        foreach ($initialParms as $parm => $value) {
-            if ($validatedParms[$parm] === false) {
-                $comment = match ($parm) {
-                    'id' => 'an non alphanumeric identification withou special characters have been generated.',
-                    'status' => 'select a pre-defined value of the filer \"stauts\"',
-                    'from', 'to' => 'incorrect time format.',
-                    'keywords' => 'keywords have not been sanitized correctly, check for special characters.',
-                    'createdBy' => 'user name should be alphanumerical only.',
-                    default => 'failed input data validation, check the system logs.'
-                };
-                throw new InvalidArgumentException("Invalid $parm format: $value, $comment");
+            foreach ($initialParms as $parm => $value) {
+                if ($validatedParms[$parm] === false) {
+                    $comment = match ($parm) {
+                        'id' => 'an non alphanumeric identification withou special characters have been generated.',
+                        'status' => 'select a pre-defined value of the filer \"stauts\"',
+                        'from', 'to' => 'incorrect time format.',
+                        'keywords' => 'keywords have not been sanitized correctly, check for special characters.',
+                        'createdBy' => 'user name should be alphanumerical only.',
+                        default => 'failed input data validation, check the system logs.'
+                    };
+                    throw new InvalidArgumentException("Invalid $parm format: $value, $comment");
+                }
             }
+            $this->_validatedParms = $validatedParms;
         }
-        $this->_validatedParms = $validatedParms;
     }
-
+    //Overrriding beforFilter to apply validation if any parameter is passed.
     #[Override]
     public function beforeFilters()
     {
@@ -87,6 +89,7 @@ class ApplicationController extends Controller
         }
     }
 
+    // Function to get the validation status of each parameter after beforeFilters is run
     public function isIdValid(): bool
     {
         return !($this->_validatedParms['id'] === false);
